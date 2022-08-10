a295fb6aa96bcbf64b7fbf5d173d52d9b4fd42bb
CmdBlock-zqg <zhuqinggangemail@qq.com>
2022-08-10 22:01:05 +0800

    test update

--- backend/test.js
+++ backend/test.js
@@ -1,43 +1,7 @@
-import Danmu from './danmu.js'
 import db from './db.js'
 import fs from 'fs'
 import { exec } from 'child_process'
 import nodejieba from 'nodejieba'
-
-const danmu = new Danmu(21402309)
-// 关注真白花音喵 关注真白花音谢谢喵
-
-danmu.connect()
-
-danmu.on('disconnect', () => {
-  console.log('* 从服务器断开连接，重连')
-  danmu.connect() // 重连
-})
-
-danmu.on('ws', () => {
-  console.log('* Websocket链接建立')
-})
-
-danmu.on('ready', () => {
-  console.log('* 成功进入直播间')
-})
-
-danmu.on('danmu', (data) => {
-  if (data.redbag !== 0) return
-  db('danmu').insert(data)
-})
-
-danmu.on('sc', (data) => {
-  wss(data)
-  db('sc').insert(data)
-})
-
-danmu.on('liveOn', (data) => {
-  db('live').insert({
-    type: 1,
-    time: data.time
-  })
-})
 
 function calcCloud(danmu) {
   let dic = {}
@@ -57,14 +21,9 @@ function calcCloud(danmu) {
   return arr.slice(0, 100)
 }
 
-danmu.on('liveOff', async (data) => {
-  await db('live').insert({
-    type: 0,
-    time: data.time
-  })
-  console.log('下播', data)
-
-  const liveData = await db('live').find({}, { sort: { time: -1 } })
+(async (data) => {
+  let liveData = await db('live').find({}, { sort: { time: -1 } })
+  liveData = liveData.slice(1)
   let liveRes = []
   for (let i = 0; i < liveData.length; i += 2) {
     liveRes.push([liveData[i + 1].time, liveData[i].time])
@@ -79,6 +38,4 @@ danmu.on('liveOff', async (data) => {
   fs.writeFileSync(`../public/data/cloud/${l}.json`, JSON.stringify(cloudData), { flag: 'w' })
   fs.writeFileSync(`../public/data/danmu/${l}.json`, JSON.stringify(danmuData), { flag: 'w' })
   fs.writeFileSync(`../public/data/sc/${l}.json`, JSON.stringify(scData), { flag: 'w' })
-
-  exec(`cd .. && git add . && git commit -m '${l}' && git push`)
-})
+})()
